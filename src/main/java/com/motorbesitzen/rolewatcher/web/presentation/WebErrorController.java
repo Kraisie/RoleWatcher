e82e990813c8e3e36500b50576f2b35c00aa3598
Kraisie <frolen307@gmail.com>
2021-01-30 09:04:08 +0100

    Changed web redirect to RedirectView

--- src/main/java/com/motorbesitzen/rolewatcher/web/presentation/WebErrorController.java
+++ src/main/java/com/motorbesitzen/rolewatcher/web/presentation/WebErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Handles the error page content.
@@ -20,9 +20,11 @@ public class WebErrorController implements ErrorController {
 	 * @return The redirection.
 	 */
 	@RequestMapping(value = "/error", method = RequestMethod.GET)
-	private ModelAndView showErrorPage() {
-		String redirectUrl = EnvironmentUtil.getEnvironmentVariableOrDefault("ERROR_REDIRECT_URL", "https://theannoyingsite.com/");
-		return new ModelAndView("redirect:" + redirectUrl);
+	private RedirectView showErrorPage() {
+		final String redirectUrl = EnvironmentUtil.getEnvironmentVariableOrDefault("ERROR_REDIRECT_URL", "https://theannoyingsite.com/");
+		final RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(redirectUrl);
+		return redirectView;
 	}
 
 	// Deprecated Spring function, do not @Override
