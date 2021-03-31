c6d33df7d49f9e9351d8edd2325e6740c9360f68
Kraisie <frolen307@gmail.com>
2021-03-31 05:42:00 +0200

    Improved error page forwarding

--- src/main/java/com/motorbesitzen/rolewatcher/web/presentation/WebErrorController.java
+++ src/main/java/com/motorbesitzen/rolewatcher/web/presentation/WebErrorController.java
@@ -3,10 +3,13 @@ package com.motorbesitzen.rolewatcher.web.presentation;
 
 import com.motorbesitzen.rolewatcher.util.EnvironmentUtil;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
 
 /**
  * Handles the error page content.
@@ -15,16 +18,14 @@ import org.springframework.web.servlet.view.RedirectView;
 public class WebErrorController implements ErrorController {
 
 	/**
-	 * Redirects to an internal or external error page set in the environment variables.
+	 * Redirects to an external error page set in the environment variables.
 	 *
 	 * @return The redirection.
 	 */
-	@RequestMapping(value = "/error", method = RequestMethod.GET)
-	private RedirectView showErrorPage() {
+	@RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.HEAD, RequestMethod.DELETE})
+	private ResponseEntity<?> showErrorPage() {
 		final String redirectUrl = EnvironmentUtil.getEnvironmentVariableOrDefault("ERROR_REDIRECT_URL", "https://theannoyingsite.com/");
-		final RedirectView redirectView = new RedirectView();
-		redirectView.setUrl(redirectUrl);
-		return redirectView;
+		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirectUrl)).build();
 	}
 
 	// Deprecated Spring function, do not @Override
