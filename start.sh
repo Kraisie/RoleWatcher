f537aa7be90793adee33d9188b79aa8ab5ecd4bc
Kraisie <frolen307@gmail.com>
2021-06-17 09:29:24 +0200

    Remove version from jar name that may break start script

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ set +a
 
 # execute the program with java to avoid gradle overhead
 echo "$(printf '\t')"Executing bot...
-java -jar ./build/libs/rolewatcher-*.jar </dev/null >/dev/null 2>&1 &
+java -jar ./build/libs/rolewatcher.jar </dev/null >/dev/null 2>&1 &
 echo $! > pid.txt
 
 echo Started the bot. The bot will appear online in a few seconds.
